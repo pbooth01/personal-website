4345019143b27df69ab7f6d90fe4cfb9d08a625a
Phil Booth <phillip.booth2015@gmail.com>
2016-04-30 20:50:02 -0400

    added working animation in intro section

--- public/js/scrollCode.js
+++ public/js/scrollCode.js
@@ -1,11 +1,20 @@
 var ScrollModule = (function(){
-  var initializeIntroScroll = function(){
+  var initializeIntroScroll = function() {
 
-    $("#intro-content h1").wrapEach(/(.)/g, "<span>$1</span>");
+    //$("#intro-content h1").wrapEach(/(.)/g, "<span>$1</span>");
+
+    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    var textWidth = $('#intro-text').outerWidth();
+    var imageWidth = $('#intro-image').outerWidth();
 
     var laola = new TimelineMax()
-      .add(TweenMax.staggerTo("#intro-content h1 span", 1, {top: -150, x: -75, scale: 3, color: '#ff4468' }, 0.7))
+      .add(TweenMax.staggerTo("#intro-content h1 span", 1, {top: -100, x: -75, scale: 3, color: '#000000'}, 0.7))
       .add(TweenMax.staggerTo("#intro-content h1 span", 1, {top: 0, x: 0, scale: 1, color: '#000000'}, 0.7), 1);
+
+    var molly = new TimelineMax()
+      .add(TweenMax.to("#intro-text", 1, {x: -(w/2 + textWidth)}))
+      .add(TweenMax.to("#intro-image", 2, {x: (w/2 + imageWidth)}));
+
 
     // init the controller
     var controller = new ScrollMagic.Controller({
@@ -17,11 +26,19 @@ var ScrollModule = (function(){
     // jumping text
     new ScrollMagic.Scene({
       duration: 200,
-      offset: 450
+      offset: 0
     })
       .setTween(laola)
       .addTo(controller);
-  };
+
+    //sliding text off of screen
+    new ScrollMagic.Scene({
+      duration: 100,
+      offset: 201
+    })
+      .setTween(molly)
+      .addTo(controller);
+  }
 
   return{
     initialize: initializeIntroScroll
